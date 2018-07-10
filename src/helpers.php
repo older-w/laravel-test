a4e98470281c2629cc24e5a8ec61803e78ba00d1
wuyujie <coder5@qq.com>
2018-07-10 17:40:09 +0800

    初始composer

---
+++ src/helpers.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: white
+ * Date: 7/10/18
+ * Time: 5:36 PM
+ */
+
+if (!function_exists('olderw_test')) {
+    function olderw_test()
+    {
+
+    }
+}
